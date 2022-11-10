0f1b25f86bcc9844a4a844e02b4251fdd534529e
Takahiro Murakami <tayuusfka@gmail.com>
2022-11-10 23:05:56 +0900

    first commit

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,7 +8,7 @@ class Navbar extends Component {
           <div className="container-fluid">
             <h1 className="text-danger h1">Only Lonely?</h1>
           </div>
-          <p><dt>This is a questionnaire that checks how lonely you feel.</dt></p>
+          <p><dt>This is a questionnaire that checks how lonely you feel!</dt></p>
         </nav>    
       );
     }
